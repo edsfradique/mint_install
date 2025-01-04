12976676740f6f4a83201dd8a9377da4044ecce3
Eduardo Fradique <edsfradique@outlook.com.br>
2025-01-04 13:17:37 -0300

    first commit

---
+++ replace_sources.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Backup do arquivo original sources.list
+echo "Criando backup do sources.list original..."
+cp /etc/apt/sources.list /etc/apt/sources.list.bak
+
+# Substituindo o conteúdo de sources.list pelos novos repositórios
+echo "Substituindo o conteúdo do sources.list..."
+cat <<EOL > /etc/apt/sources.list
+# Repositórios do Mint e Debian
+deb https://mirror.ufscar.br/mint-archive faye main upstream import backport
+deb http://debian.pop-sc.rnp.br/debian bookworm main contrib non-free non-free-firmware
+deb http://debian.pop-sc.rnp.br/debian bookworm-updates main contrib non-free non-free-firmware
+deb http://security.debian.org/ bookworm-security main contrib non-free non-free-firmware
+deb http://debian.pop-sc.rnp.br/debian bookworm-backports main contrib non-free non-free-firmware
+EOL
+
+# Atualizando o sistema com os novos repositórios
+echo "Atualizando a lista de pacotes..."
+apt update
+
+echo "Substituição do repositório concluída com sucesso!"
